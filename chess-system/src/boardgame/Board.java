a6cbb059c430b03e37cb3f87bdca7613039a018e
Isabella Maria Navarro Beneveni Smith <Isabella.maria@venturus.org.br>
2021-10-20 10:29:32 -0300

    Moving pieces

--- chess-system/src/boardgame/Board.java
+++ chess-system/src/boardgame/Board.java
@@ -36,6 +36,19 @@ public class Board {
 		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
+	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
 
 	public void placePiece(Piece piece, Position position) {
 		if (thereIsAPiece(position)) {
