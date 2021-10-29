c622fea902b3ab6ea3bccab631ba0bdab0979ad4
Isabella Maria Navarro Beneveni Smith <Isabella.maria@venturus.org.br>
2021-10-29 16:32:06 -0300

    Bishop and fix on Pawn

--- chess-system/src/chess/pieces/Pawn.java
+++ chess-system/src/chess/pieces/Pawn.java
@@ -12,6 +12,11 @@ public class Pawn extends ChessPiece {
 	}
 
 	@Override
+	public String toString() {
+		return "P";
+	}
+	
+	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
@@ -58,10 +63,4 @@ public class Pawn extends ChessPiece {
 		}
 		return mat;
 	}
-	
-	@Override
-	public String toString() {
-		return "P";
-	}
-
 }
